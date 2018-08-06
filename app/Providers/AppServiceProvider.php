80fab4c85792309990dec0bf8f82030b51cc8285
rmit-s3604380-Ronit-Patel <s3604380@student.rmit.edu.au>
2018-08-06 16:47:35 +1000

    resolve ronit local error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,9 +11,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
-
     /**
      * Register any application services.
      *
