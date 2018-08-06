860fd87669db7209519927f0a1c0300ad266cde7
Linhngg <linhng26@gmail.com>
2018-08-06 18:21:06 +1000

    Add logout for navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,6 +127,14 @@
             <div class="top-right links">
                 @auth
                     <a href="{{ url('/home') }}">Home</a>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                    Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 @else
                     <a href="{{ route('login') }}">Login</a>
                     <a href="{{ route('register') }}">Register</a>
