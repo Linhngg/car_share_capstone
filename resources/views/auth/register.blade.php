f4a95b9e3ce48038fa2f77b88e1d6fb0334ee84e
Linhngg <linhng26@gmail.com>
2018-08-09 20:46:57 +1000

    Update for login register and home

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,183 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('assets')
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    {{--<div class="container">--}}
+        {{--<div class="row justify-content-center">--}}
+            {{--<div class="col-md-8">--}}
+                {{--<div class="card">--}}
+                    {{--<div class="card-header">{{ __('Register') }}</div>--}}
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-                        @csrf
+                    {{--<div class="card-body">--}}
+                        {{--<form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">--}}
+                            {{--@csrf--}}
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>--}}
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="name" type="text"--}}
+                                           {{--class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"--}}
+                                           {{--name="name" value="{{ old('name') }}" required autofocus>--}}
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
+                                    {{--@if ($errors->has('name'))--}}
+                                        {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $errors->first('name') }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="email"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="email" type="email"--}}
+                                           {{--class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"--}}
+                                           {{--name="email" value="{{ old('email') }}" required>--}}
+
+                                    {{--@if ($errors->has('email'))--}}
+                                        {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="password"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="password" type="password"--}}
+                                           {{--class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"--}}
+                                           {{--name="password" required>--}}
+
+                                    {{--@if ($errors->has('password'))--}}
+                                        {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $errors->first('password') }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="password-confirm"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="password-confirm" type="password" class="form-control"--}}
+                                           {{--name="password_confirmation" required>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row mb-0">--}}
+                                {{--<div class="col-md-6 offset-md-4">--}}
+                                    {{--<button type="submit" class="btn btn-primary">--}}
+                                        {{--{{ __('Register') }}--}}
+                                    {{--</button>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-head">{{ __('Sign up') }}</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                            @csrf
+
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                       <img src="{{url('/images/user.png')}}" alt="Image User"/>
+                                    </span>
+                                    </div>
+                                    <input id="name" type="text" placeholder="Name"
+                                           class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                           name="name" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                       <img src="{{url('/images/mail.png')}}" alt="Image Envelope"/>
+                                    </span>
+                                    </div>
+                                    <input id="email" type="email" placeholder="Email Address"
+                                           class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                           name="email" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                       <img src="{{url('/images/closed-lock.png')}}" alt="Image password"/>
+                                    </span>
+                                    </div>
+                                    <input id="password" type="password" placeholder="Password "
+                                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                           name="password" required>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                       <img src="{{url('/images/closed-lock.png')}}" alt="Image password"/>
+                                    </span>
+                                    </div>
+                                    <input id=password-confirm" type="password" placeholder="Repeat password "
+                                           class="form-control" name="password_confirmation" required>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="btnLogin btn-primary form-control">
+                                {{ __('Submit') }}
+                            </button>
+                            <label class=" col-form-label text-md-right">{{ __('Already have account?') }}</label> <a
+                                    href="{{ route('login') }}">Login</a>
+                        </form>
+                    </div>
+
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,72 @@
 @extends('layouts.app')
 
+@section('assets')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-12">
+            {{--<div class="card">--}}
+                {{--<div class="card-header">Dashboard</div>--}}
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                {{--<div class="card-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success" role="alert">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
 
-                    You are logged in!
-                </div>
-            </div>
+                    {{--You are logged in!--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            <div id="map"></div>
+            <script>
+                // Note: This example requires that you consent to location sharing when
+                // prompted by your browser. If you see the error "The Geolocation service
+                // failed.", it means you probably did not give permission for the browser to
+                // locate you.
+                var map, infoWindow;
+                function initMap() {
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: {lat: -34.397, lng: 150.644},
+                        zoom: 6
+                    });
+                    infoWindow = new google.maps.InfoWindow;
+
+                    // Try HTML5 geolocation.
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function(position) {
+                            var pos = {
+                                lat: position.coords.latitude,
+                                lng: position.coords.longitude
+                            };
+
+                            infoWindow.setPosition(pos);
+                            infoWindow.setContent('Location found.');
+                            infoWindow.open(map);
+                            map.setCenter(pos);
+                        }, function() {
+                            handleLocationError(true, infoWindow, map.getCenter());
+                        });
+                    } else {
+                        // Browser doesn't support Geolocation
+                        handleLocationError(false, infoWindow, map.getCenter());
+                    }
+                }
+
+                function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+                    infoWindow.open(map);
+                }
+            </script>
+            <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPWvU7gcEWfLVo5nFPBbM9AL3KfYHqTaU&callback=initMap">
+            </script>
         </div>
     </div>
 </div>
