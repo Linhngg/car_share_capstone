0a72f91e2f4db9cb04e94b0b74a5adc61dd621b8
Linhngg <linhng26@gmail.com>
2018-08-14 00:08:19 +1000

    https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,7 +8,10 @@ class HttpsProtocol
 {
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'prod') {
+//        if (!$request->secure() && env('APP_ENV') === 'prod') {
+//            return redirect()->secure($request->getRequestUri());
+//        }
+        if (env('APP_ENV') === 'prod') {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
