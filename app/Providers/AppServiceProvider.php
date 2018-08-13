4d3cfc23c8455c5c500bdae5d37b41083c04abfd
Linhngg <linhng26@gmail.com>
2018-08-14 00:18:24 +1000

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'prod') {
             $this->app['url']->forceScheme('https');
         }
 //        $this->app['url']->forceScheme('https');
