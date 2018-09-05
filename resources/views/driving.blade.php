3a9b393eb91c28bfe6bced3d3b98a6f946868888
rmit-s3604380-Ronit-Patel <s3604380@student.rmit.edu.au>
2018-09-05 13:25:06 +1000

    Confirmation page
    created confirmation page , requires some work for the scroll bar on the right hand side , image size

--- resources/views/driving.blade.php
+++ resources/views/driving.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -58,18 +59,47 @@
             text-decoration: none;
             text-transform: uppercase;
         }
+
+        .img-wrapper {
+            position: relative;
+        }
+
+        .img-responsive {
+            width: 100%;
+            height: 100%;
+        }
+
+        .img-overlay {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            text-align: center;
+        }
+
+        .img-overlay:before {
+            content: ' ';
+            display: block;
+            /* adjust 'height' to position overlay content vertically */
+            height: 60%;
+        }
     </style>
 </head>
 <body>
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title">
-            Ready to Drive now!
         </div>
-        {!! Form::open(['method' => 'POST', 'route' => 'return']) !!}
-        {{ Form::hidden('id', $car->id) }}
-        <button type="submit" class="btn float-right btn-lg btn-dark d-block " id="bookBtn">Return</button>
-        {!! Form::close() !!}
+        <div class='img-wrapper'>
+            <img class="img-responsive" src="{{url('/images/38831510_2313791305304283_3555259866182320128_n (1).jpg')}}" alt="Girl in a jacket">
+            <div class="img-overlay">
+                <button type="button" class="btn btn-light btn-lg">Return</button>
+            </div>
+        </div>
+         {!! Form::open(['method' => 'POST', 'route' => 'return']) !!}
+    {{ Form::hidden('id', $car->id) }}
+    {!! Form::close() !!}
     </div>
 </div>
 </body>
