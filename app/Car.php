b10dedb169c1df13ddb33c91b991b5a949948fbd
Linhngg <linhng26@gmail.com>
2018-09-11 14:53:49 +1000

    Adding Odometer migration, seed data, display value to admin.

--- app/Car.php
+++ app/Car.php
@@ -7,8 +7,50 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     protected $fillable = [
-        'id', 'model', 'status', 'lat', 'long', 'updated_at'
+        'id', 'model', 'status', 'lat', 'long', 'updated_at', 'odometer', 'lastServiceOdometer', 'serviceInterval', 'lastServiceDate'
     ];
+
+    public function getReadableStatusAttribute()
+    {
+        $readableStatus = 'Default';
+        switch ($this->status) {
+            case 0:
+                $readableStatus = 'Booked';
+                break;
+            case 1:
+                $readableStatus = 'Available';
+                break;
+            case 2:
+                $readableStatus = 'Servicing';
+                break;
+            case 3:
+                $readableStatus = 'Retired';
+                break;
+            default:
+
+        };
+        return $readableStatus;
+    }
+
+    public function getServiceStatusAttribute()
+    {
+        $nextService = $this->lastServiceOdometer + $this->serviceInterval;
+        $different = $nextService - $this->odometer;
+        $readableStatus = 'Default';
+        switch ($different) {
+            case ($different < 0):
+                $readableStatus = 'Need Service!';
+                break;
+            case ($different > 0 && $different < 1000):
+                $readableStatus = 'Close to Service';
+                break;
+            case ($different > 1000):
+                $readableStatus = 'OK';
+                break;
+            default:
+        };
+        return $readableStatus;
+    }
 
     public function book()
     {
